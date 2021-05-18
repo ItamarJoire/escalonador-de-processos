198dac1b00469f2aec4938629884f0042c18be45
Itamar Joire <ijoire7@gmail.com>
2021-05-18 11:19:26 -0300

    prioridades ordenadas

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-// const elementShowProcess = document.querySelector('.show-process');
-// const processFifo = document.querySelector('.show-fifo');
-
-// function fifo(){
-//     let processNumber = elementShowProcess.value;
-
-//     const newForm = document.createElement('div');
-//     newForm.innerHTML = createStruct(newForm);
-
-//     processFifo.appendChild(newForm);
-
-//     console.log(processFifo);
-
-// }  
-
-// function createStruct(html){
-//     html = `
-//         <h3>Tempo de chegada</h3>
-//         <input type="number">
-
-//         <h3>Tempo de execução</h3>
-//         <input type="number">
-//     `
-//     return html;
-// }
-// var notas = new Array(n);
-
-// const elementShowProcess = 
-// const n = elementShowProcess.value;
-
-// const arrayForm = new Array();
-
-// const arrayForm = new Array
-
 const processesVector = [];
 
 const dom = {
@@ -44,12 +10,10 @@ const dom = {
             let newForm = document.createElement('div');
             newForm.setAttribute('class', 'form');
             newForm.innerHTML = this.createStruct(newForm);
+            
             let struct = this.structForm.appendChild(newForm);
-            processesVector.push(struct);
+            processesVector.push({struct});
         }
-
-        this.displayProcess();
-       
     },
 
     createStruct(html){
@@ -62,35 +26,23 @@ const dom = {
         `
         return html;
     },
-
-    displayProcess(){
-            console.log(processesVector);
-    }
 }
 
-
-const arrayValue = [];
-
-const process = {   
- 
+const process = {
     startingProcesses(){
-        for(let i = 0; i < processesVector.length; i++){
-            let valueTimeInit = processesVector[i].querySelector('#time-init');
-            arrayValue.push(valueTimeInit.value);  
-           
-            console.log(valueTimeInit.value);
+        for(let i = 0; i < processesVector.length; i++){ 
+            let element = processesVector[i].struct.querySelector('#time-init').value;  
+            processesVector[i].priority = element;
         }
 
-        console.log(this.priorityProcess(arrayValue));
+        this.orderProcesses(console.log(processesVector));
+        
     },
 
-    // Pega o processo que tem o menor tempo de chegada
-    priorityProcess(array){
-        return Math.min.apply(Math, array);
-    }
+    orderProcesses(){
+        processesVector.sort((a, b) => {
+            return a.priority - b.priority;
+        })
+    },
 
 }
-
-
-
-    
